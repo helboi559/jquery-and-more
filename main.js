83d66974322d165b66b1ac6d4a11255c1b2729d1
Juan Ramirez <rami8224.2007@gmail.com>
2022-04-04 14:15:11 -0400

    challenege 1 done

--- main.js
+++ main.js
@@ -10,28 +10,34 @@ div.on("click", ()=> {
     alert('Hello World')
 })
 
-//create a chart
+//DEFINE chart elements
+const labels = [
+'January',
+'February',
+'March',
+'April',
+'May',
+'June',
+];
 
-let labels = ['January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June'];
-
-let  data = {
-    labels:labels,
-    dataSets: [{
-        label:"my first dataset",
-        backgroundColor: "rgb(255,99,132)",
-        borderColor: "rgb(255,99,132)",
-        data: [0,10,5,2,20,30,45]
-    }]
-};  
-
-const config = {
-    type:"line",
-    data:data,
-    options: {}
+const data = {
+labels: labels,
+datasets: [{
+    label: 'My First dataset',
+    backgroundColor: 'rgb(255, 99, 132)',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [0, 10, 5, 2, 20, 30, 45],
+}]
 };
 
+const config = {
+type: 'line',
+data: data,
+options: {}
+};
+
+//RENDER/CREATE Chart
+const myChart = new Chart(
+document.getElementById('myChart'),
+config
+);
